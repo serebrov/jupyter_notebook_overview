679a0ff12336942b94abc6666b7ce8f29eea9b93
serebrov <serebrov@gmail.com>
2018-01-12 15:39:46 +0200

    Move requirements out from the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 virtualenv -p python3.5 venv
 venv/bin/pip install --upgrade pip
-venv/bin/pip install jupyter
-venv/bin/pip install matplotlib
-venv/bin/pip install sympy
+venv/bin/pip install -r requirements.txt
